cd469e86a9444d7474cdda15e2a1a91c76dc1ce6
moarshad100 <mo.arshad100@gmail.com>
2023-08-03 15:05:35 +1000

    new javascript files

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 
-const questions = ["what is you shape","whats your color"];
+const questions = [
+	"Please enter three letters: ",
+	"Enter the text color: ",
+	"Select the shape: ",
+	"Enter the Shape color: ",
+	];
 
 inquirer.prompt([
 	{
 	type:'input',
 	message:questions[0],
-	name:'shape'
+	name:'letters'
 	},
 
 	{
 	type:'input',
 	message:questions[1],
-	name:'color'
+	name:'text_color'
 	},
+	{
+	type:'list',
+	message:questions[2],
+	name:'shape',
+	choices:["circle","square","triangle"]
+	},
+	{
+	type:'input',
+	message:questions[3],
+	name:'shape_color'
+	}
 	
 
 	])
